a1d81a02913fb9a3d589bd6ec5052a2b825ffd3c
Cainan Beltrame <39142115+Cainan94@users.noreply.github.com>
2025-08-11 21:03:04 -0300

    initial commit

---
+++ src/main/java/com/cainanbt/softwares/controleja/ControlejaApplication.java
@@ -0,0 +1,13 @@
+package com.cainanbt.softwares.controleja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cainanbt/softwares/controleja/ControlejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cainanbt.softwares.controleja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
